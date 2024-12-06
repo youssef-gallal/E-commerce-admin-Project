332366cc3cabf21b29e3e5c481ae6fdc404bd2eb
youssefgalal <youssefgalal103@gmail.com>
2024-12-07 00:59:33 +0200

    first sidebar

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [SidebarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+
+export class AppComponent {
+
+}
+
