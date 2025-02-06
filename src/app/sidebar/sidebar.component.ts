e79cac90cdb0bd56642b19f52a1fd39b484938b4
youssefgalal <youssefgalal103@gmail.com>
2025-02-06 14:12:13 +0200

    orders change

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,17 +6,23 @@ import { FormsModule } from '@angular/forms';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterModule } from '@angular/router';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatSidenavModule, MatIconModule, MatSidenavContainer, FormsModule, MatExpansionModule, MatButtonModule, MatListModule, RouterLink, RouterModule,],
+  imports: [MatSidenavModule, CommonModule, MatIconModule, MatSidenavContainer, FormsModule, MatExpansionModule, MatButtonModule, MatListModule, RouterLink, RouterModule,],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
   isMinimized = false;
+  isDropdownOpen = false;
 
   toggleMinimize() {
     this.isMinimized = !this.isMinimized;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 }
